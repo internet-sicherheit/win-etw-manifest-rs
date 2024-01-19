26f95dfdd40bc50b7450334571e37c18d4e1052e
Marvin Gudel <github.marvin@gudel.org>
2024-01-19 15:44:03 +0100

    Add macro which filters all files from input path

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod parser;
 
-use std::{fs::File, path::Path};
+use std::{
+    fs::{read_dir, DirEntry, File},
+    path::Path,
+};
 
 use proc_macro::TokenStream;
 
@@ -20,6 +23,26 @@ pub fn parse_manifest(input: TokenStream) -> TokenStream {
     let _provider = parse(&mut f).unwrap();
 
     TokenStream::new()
+}
+
+#[proc_macro]
+pub fn include_manifests(input: TokenStream) -> TokenStream {
+    let input = match input.into_iter().next().unwrap() {
+        proc_macro::TokenTree::Literal(x) => x.to_string(),
+        _ => panic!("Input must be path literal"),
+    };
+    let str_path = input.trim_matches('"');
+    let path = Path::new(&str_path);
+
+    // read all files from path
+    let _files: Vec<_> = read_dir(path)
+        .unwrap()
+        .filter_map(|x| x.ok())
+        .filter(|x| x.file_type().is_ok_and(|x| x.is_file()))
+        .map(|x| x.path())
+        .collect();
+
+    todo!()
 }
 
 #[cfg(test)]
